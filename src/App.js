9cd5353b09bd06277a80810ced37134403697ae7
kaphley <amritkaphle12@gmail.com>
2021-08-11 15:45:16 +0530

    add picture

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import { Button, Container } from "react-bootstrap";
+import { Button, Container, Carousel } from "react-bootstrap";
 import Navbar from "./components/Navbar";
+import Lorem, { LoremIpsum } from "react-lorem-ipsum";
+import Img1 from "./assets/hungryChild.jpg";
+import Img2 from "./assets/hungryChild2.webp";
+import Img3 from "./assets/hugryChild3.jpg";
+import Img4 from "./assets/hungryChild4.jpg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Container
-        style={{
-          height: "100vh",
-          background: "black",
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center'
-        }}
-      >
-        <Button variant="success">Donate</Button>
-      </Container>
+
+      <Carousel>
+        <Carousel.Item>
+          <img className="carouselImg" src={Img1} alt="hungry child" />
+          <Carousel.Caption>
+            <h3>Lorem ipsum odor amet, consectetuer</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+
+        <Carousel.Item>
+          <img className="carouselImg" src={Img2} alt="hungry child" />
+          <Carousel.Caption>
+            <h3>Lorem ipsum odor amet, consectetuer</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="carouselImg" src={Img3} alt="hungry child" />
+          <Carousel.Caption>
+            <h3>Lorem ipsum odor amet, consectetuer</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="carouselImg" src={Img4} alt="hungry child" />
+          <Carousel.Caption>
+            <h3>Lorem ipsum odor amet, consectetuer</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+        <Button className = 'donateBtn'
+        variant="success"
+       
+        >Donate</Button>
     </div>
   );
 }
 
 export default App;
+// Lorem ipsum odor amet, consectetuer adipiscing elit. Augue bibendum senectus felis varius curabitur. Montes ipsum dictumst orci volutpat quisque etiam torquent integer. Senectus proin et nisi aliquam scelerisque ultricies faucibus finibus. Vehicula neque gravida luctus iaculis fringilla. Tincidunt aptent nascetur nunc sed parturient class tristique. Cras pulvinar laoreet phasellus eros pellentesque aliquam vel dapibus. Penatibus vehicula gravida per dignissim quis quisque luctus. Semper dapibus ut efficitur; sollicitudin ligula est nascetur magnis pharetra. Dapibus id maximus metus lobortis nascetur iaculis duis.
